afdf40cdaf438992ca92f8627769ffab1e6e4472
bimal <bimal@email.com>
2024-03-24 13:24:25 +0530

    search

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,14 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    query = request.GET.get('q')
+    if query:
+        posts = Post.objects.filter(title__icontains=query, published_date__lte=timezone.now()).order_by('published_date')
+        if not posts:
+            message = "No such blog found."
+            return render(request, 'blog/post_list.html', {'message': message})
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
